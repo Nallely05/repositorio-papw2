625919687250c6ff264e170e078ed4a76d1310f5
Nallely05 <47049131+Nallely05@users.noreply.github.com>
2019-03-12 14:36:55 -0600

    correccionLogo

--- resources/views/navbarNormal.blade.php
+++ resources/views/navbarNormal.blade.php
@@ -7,8 +7,7 @@ $Busqueda=$_GET['Buscar'];
 $Busqueda=false;
 ?>
 <header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar" style="background: rgb(102, 59, 201);">
-  <a class="navbar-brand mr-0 mr-md-2" href="/" aria-label="Bootstrap"><img src="../Images/iconoPeque1.png" width="30" height="30" alt=""></a>
-
+  <a class="navbar-brand mr-0 mr-md-2" href="/" aria-label="Bootstrap"><img src="../Images/IconoPeque1.png" width="30" height="30" alt=""></a>
 
   <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
     @if($Busqueda)
