883a556a21eaac4c08e7f8f82f41f77c5a24403f
Nallely05 <47049131+Nallely05@users.noreply.github.com>
2019-05-04 00:16:02 -0500

    des-error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::deafultStringLength(191);
+        //Schema::deafultStringLength(191);
     }
 
     /**
